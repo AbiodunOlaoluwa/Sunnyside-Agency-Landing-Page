5361f61dcc2ed70a6898b2b072213b243fd563f1
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2023-12-05 23:00:56 +0100

    finished the coding for the desktop site

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-// setTimeout(
-//     document.querySelector(".arrowContainer").classList.add("bounce")
-// , 1000)
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 10;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
